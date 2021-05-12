377a3f4d349d1ab180658e1ca9eda8055f3da95f
Oliver Joseph Ash <oliverjash@gmail.com>
2021-05-12 13:51:36 +0100

    Init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { getErrors } from './state';
+
+export const fn = () => {
+    const state = getErrors();
+
+    const test = { state };
+};
---
+++ src/state.ts
@@ -0,0 +1,3 @@
+import * as t from 'io-ts';
+
+export declare const getErrors: () => t.Errors;
